d406c71e647a326310dcea4ac809df822ba2f363
chand-stack <chand.stack@gmail.com>
2023-08-04 04:55:14 +0600

    js practice

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
-function deviNum(num){
-    let devidedNumber = num % 5;
-    return devidedNumber;
-}
+function mark(num){
 
-let sara = 119;
-const devidedNum = deviNum(sara);
-console.log(devidedNum);
+    if(num >= 80 && num < 90){
+        return 'grade A'
+    }else if(num >= 90){
+        return 'grade A+'
+    }
+    else if(num >= 70 && num < 80){
+    return 'grade B'}
+    else if(num >= 60 && num < 70){
+    return 'grade C'}
+    else if(num >= 50 && num < 60){
+        return ' grade D'
+    }
+    else if (num < 50){
+        return ' grade F'
+    }
+
+}
+let alia = 95;
+let aliaGet = mark(alia);
+console.log('Alia get',aliaGet);
+
+let dalia = 66;
+let daliaGet = mark(dalia);
+console.log('Dalia get',daliaGet);
+
+let salia = 80;
+let saliaGet = mark(salia);
+console.log('Salia get', saliaGet);
+
+let malia = 59;
+let maliaGet = mark(malia);
+console.log('Malia get', maliaGet)
+
+let lilia = 47;
+let liliaGet = mark(lilia);
+console.log('Lilia get',liliaGet)
+
+let jalaiya = 77;
+let jalaiyaGet = mark(jalaiya);
+console.log('Jalaiya Get',jalaiyaGet)
