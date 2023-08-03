710d688c960081ac5c9c980b982007fcc20cee63
chand-stack <chand.stack@gmail.com>
2023-08-04 05:07:56 +0600

    js practice

--- app.js
+++ app.js
@@ -1,42 +1,23 @@
-function mark(num){
-
-    if(num >= 80 && num < 90){
-        return 'grade A'
-    }else if(num >= 90){
-        return 'grade A+'
-    }
-    else if(num >= 70 && num < 80){
-    return 'grade B'}
-    else if(num >= 60 && num < 70){
-    return 'grade C'}
-    else if(num >= 50 && num < 60){
-        return ' grade D'
-    }
-    else if (num < 50){
-        return ' grade F'
-    }
-
+function traffic(light){
+if(light === 'Red'){
+    return 'you are in danger'
 }
-let alia = 95;
-let aliaGet = mark(alia);
-console.log('Alia get',aliaGet);
+else if (light === 'Yellow'){
+    return 'you have to stop'
+}
+else if(light === 'Green'){
+    return 'you can go now'
+}
+}
 
-let dalia = 66;
-let daliaGet = mark(dalia);
-console.log('Dalia get',daliaGet);
+let firstLight = 'Red'
+let redlight = traffic(firstLight);
+console.log('light is red',redlight);
 
-let salia = 80;
-let saliaGet = mark(salia);
-console.log('Salia get', saliaGet);
+let secondLight = 'Yellow'
+const yellowLight = traffic(secondLight);
+console.log('light is yellow',yellowLight);
 
-let malia = 59;
-let maliaGet = mark(malia);
-console.log('Malia get', maliaGet)
-
-let lilia = 47;
-let liliaGet = mark(lilia);
-console.log('Lilia get',liliaGet)
-
-let jalaiya = 77;
-let jalaiyaGet = mark(jalaiya);
-console.log('Jalaiya Get',jalaiyaGet)
+let thirdLight = 'Green'
+const greenLight = traffic(thirdLight);
+console.log('light is green',greenLight);
