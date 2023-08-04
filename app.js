e9a5209eef395a0d68bb56c892fec08cef01b21d
chand-stack <chand.stack@gmail.com>
2023-08-04 19:05:22 +0600

    js practice

--- app.js
+++ app.js
@@ -1,27 +1,6 @@
+const pass ='chandRahman';
+const repass = 'ChandRahman';
 
-
-function signal(num){
-
-
-
-    if(num==='red'){
-        return 'you are in danger'
-    }else if(num==='yellow'){
-        return 'you have to stop'
-    }else if(num==='green'){
-        return 'you can go now'
-    }
-}
- let   light1= 'red';
- let light2= 'yellow';
-  let  light3= 'green'
-
-
-const totalsignal = signal(light1);
-console.log(totalsignal);
-
-const totalsignal1 = signal(light2)
-console.log(totalsignal1);
-
-const totalsignal2 = signal(light3);
-console.log(totalsignal2);
+if(pass.toUpperCase === repass.toUpperCase){
+    console.log('password is right')
+}else{console.log('password invalid')}
