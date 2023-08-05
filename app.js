9e08599b3a2255f329fa03ee7c3ef501ab2fd9e8
chand-stack <chand.stack@gmail.com>
2023-08-06 02:27:07 +0600

    js practice

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
+function onlypositive(numberArr){
+  const positive=[]
 
-function bestFriend(nameArr){
-
-    let myFriend = ''
-
-    for(let i = 0; i<nameArr.length; i++){
-        const element = nameArr[i];
-        if(element.length>myFriend.length){
-            myFriend = element
-        }
+  for(let i = 0; i<numberArr.length; i++){
+    const element = numberArr[i];
+    if(element>=0){
+        positive.push(element);
+    }else if(element<0){
+        break;
     }
-return myFriend;
+  }
+  return positive;
 }
 
-let namelist = ['sajid', 'mamun', 'kamal','chinku', 'pinku','chand rahman', 'munna']
 
-const finalName = bestFriend(namelist)
-console.log(finalName);
+
+const numArr = [45, 87, 96, 11, 63, -56, 71, 28];
+
+const positiveArr = onlypositive(numArr);
+console.log(positiveArr);
 
