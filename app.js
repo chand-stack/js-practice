83445166ace164eb0f5bf51ffb088a7598396578
chand-stack <chand.stack@gmail.com>
2023-08-06 01:16:07 +0600

    js practice

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-
-function uniquename(arr){
-    const uniqArray=[];
-
-    for(let i = 0; i<arr.length; i++){
-        const element= arr[i];
-        if(uniqArray.includes(element)===false){
-            uniqArray.push(element);
-        }
-    }
-return uniqArray;
-
+function centimeterToMeter(num){
+let total = num/100;
+return total;
 }
 
-const names = ['abul', 'babul', 'cabul', 'dabul', 'ebul', 'babul', 'abul', 'kabul', 'gabul', 'cabul', 'babul', 'abul', 'abul']
-const result = uniquename(names);
-console.log(result);
+const totalMeter= centimeterToMeter(100);
+console.log(totalMeter)
