1df7b9cbdeb2a056d61c3a34463ce0136b26eb50
chand-stack <chand.stack@gmail.com>
2023-08-06 02:12:14 +0600

    js practice

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-function paperRequirements(book1,book2,book3){
 
-    let paperbook1 = 100;
-    let paperbook2 = 200;
-    let paperbook3 = 300;
+function bestFriend(nameArr){
 
-    let firstBookPages = paperbook1 *book1;
-    let secondBookPages= paperbook2 *book2;
-    let thirdBookPages= paperbook3 *book3;
+    let myFriend = ''
 
-    const totalBookPages = firstBookPages + secondBookPages + thirdBookPages;
-
-    return totalBookPages;
-    
-
-
+    for(let i = 0; i<nameArr.length; i++){
+        const element = nameArr[i];
+        if(element.length>myFriend.length){
+            myFriend = element
+        }
+    }
+return myFriend;
 }
 
-let firstBook = 3;
-let secondBook = 2;
-let thirdBook = 4;
+let namelist = ['sajid', 'mamun', 'kamal','chinku', 'pinku','chand rahman', 'munna']
 
-const totalPages = paperRequirements(firstBook,secondBook,thirdBook)
-console.log(totalPages);
+const finalName = bestFriend(namelist)
+console.log(finalName);
+
