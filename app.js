06adc28036b02e2d4a0656333e405ed57c58ae1d
chand-stack <chand.stack@gmail.com>
2023-08-07 04:43:48 +0600

    js practice

--- app.js
+++ app.js
@@ -1,43 +1,9 @@
+const lyrics ='tumi bondhu kala pakhi ami jeno ki. bosonto kale tomay bolte parini'
 
-function counting(line){
+const searchString= 'Pakhi'
+const lyricsLower = lyrics.toLowerCase();
+const doesExist = searchString.toLowerCase();
 
-    let parts = line.split('');
-    let vowelWord = []
+const result = lyricsLower.includes(doesExist);
 
-    for(let i = 0; i<parts.length; i++){
-        const element = parts[i];
-        
-        if(element==='a' || element==='e' || element==='i' || element==='o' || element==='u'){
-            vowelWord.push(element);
-        }
-        
-    }
-    return vowelWord 
-}
-
-function consonentCount(lines){
-    let parts = lines.split('');
-    let consonentWord = []
-
-
-    for(let i = 0; i<parts.length; i++){
-        const element = parts[i];
-        
-         if (element!=='a' && element!=='e' && element!=='i' && element!=='o' && element!=='u' && element!== ' '){
-            consonentWord.push(element)
-        }
-        
-    }
-    return consonentWord
-
-}
-
-
-
-const lyric = 'tumi bondhu kala pakhi ami jeno ki bososnto. batashe tomay bolte parini'
-const total = counting(lyric);
-console.log('Vowel Words are: ',total);
-
-
-const consonentTotal = consonentCount(lyric)
-console.log('Consonent Words are: ',consonentTotal);
+console.log(result);
