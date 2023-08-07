89640adce5620e2be8b72e608ece7d15730260ab
chand-stack <chand.stack@gmail.com>
2023-08-08 00:21:41 +0600

    js practice

--- even.js
+++ even.js
@@ -1,15 +1,23 @@
-function isLGSeven(num){
-    if(typeof num !== "number"){
-        return 'Please provide a number'
-    }else{
-    let differ = num - 7;;
-    if(differ<7){
-        return differ
-    }else{
-        return num*2;
+function findingBadData(numArray){
+
+    if(Array.isArray(numArray)===false ){
+        return 'please enter an Array'
     }
+    else{
+        let badData = 0;
+        for(let i = 0; i<numArray.length; i++){
+            let elements = numArray[i];
+            if(typeof elements !== "number"){
+                return 'please enter an Array of numbers'
+            }
+            else if(elements<0){
+                badData++;
+            }
+            
+        }return badData;
     }
 
 }
 
-console.log(isLGSeven(6))
+
+console.log(findingBadData([2,-5,-7,-23, -5, -8]))
