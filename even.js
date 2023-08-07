c3083856a4cc60b3be8c11a19a1e7b1069147e56
chand-stack <chand.stack@gmail.com>
2023-08-07 16:49:09 +0600

    js practice

--- even.js
+++ even.js
@@ -1,6 +1,24 @@
-var num=2;
+const shoppingCart = [
+    { name : 'shoe', price:1200},
+    { name:'shirt', price:2200},
+    { name:'pant', price:3700},
+    {name:'belt', price:600}
 
-while(num<=20){
-    console.log(num);
-    num = num+2;
+]
+
+function totalCost(shoppingList){
+    let expense = 0;
+
+    for(let i =0; i<shoppingList.length; i++){
+        const elements = shoppingList[i];
+        expense = expense+elements.price;
+    }
+
+    return expense;
+
+
+
 }
+
+const totalExpense = totalCost(shoppingCart);
+console.log(totalExpense);
