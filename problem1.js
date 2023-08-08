b1eeaf315b61724d76f8e22b17f3906d03af2d85
chand-stack <chand.stack@gmail.com>
2023-08-08 19:06:38 +0600

    js practice

---
+++ problem1.js
@@ -0,0 +1,12 @@
+function cubeNumber(number) {
+
+    if(typeof number !== "number"){
+        return 'Please input a Number'
+    }else if(number<0){
+        return 'Please input a positive Number'
+    }
+    const result = number*number*number;
+    return result
+}
+
+console.log(cubeNumber(4))
