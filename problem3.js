d721de5cfcb46b9b841a5f63c3651b641a63e7fc
chand-stack <chand.stack@gmail.com>
2023-08-08 21:34:20 +0600

    js practice

---
+++ problem3.js
@@ -0,0 +1,26 @@
+function sortMaker(arr) {
+    if (!Array.isArray(arr) || arr.length !== 2) {
+        return 'Please Enter an Array'
+    }
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        // console.log(element);
+        if (element < 0) {
+            return 'please Enter a number of Array'
+        }
+    }
+
+    if (arr[0] === arr[1]) {
+        return "Equal"
+    }
+
+    let x = []
+    x.push(Math.max(...arr))
+    x.push(Math.min(...arr))
+    return x
+
+}
+
+
+const output = sortMaker([4, 4]);
+console.log(output);
