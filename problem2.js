39948ac624d34d5555657385f36f886bce7d56cd
chand-stack <chand.stack@gmail.com>
2023-08-08 19:28:06 +0600

    js practice

---
+++ problem2.js
@@ -0,0 +1,13 @@
+function matchFinder(string1, string2) {
+    if(typeof string1 !== "string" || typeof string2!== "string"){
+        return 'Please enter a string'
+    }else{
+        if(string1.includes(string2)){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+}
+console.log(matchFinder('John Doe', 'ohn'))
