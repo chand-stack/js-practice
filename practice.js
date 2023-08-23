1f0d6cc9819677c8023ce756b0a402cb94b09948
chand-stack <chand.stack@gmail.com>
2023-08-24 01:14:08 +0600

    js practice

--- practice.js
+++ practice.js
@@ -1,25 +1,22 @@
-let students = {
-   name: "jack",
-   section : "C",
-   class : "IX",
-   address : {
-      "building no": 12,
-      "street": "St. jonson",
-      "city": "Petersburg",
-      "country": "UK"
-   },
-   3333:{
-      name:"herry",
-      section: "D",
-      class : "X",
-      address : {
-         "building no" : 85,
-         "street" : "DC road",
-         "city" : "Kachukhet",
-         "country" : "Bangladesh"
+let data = {
+   data:[
+      {
+         bookId : 1,
+         bookDetails : {
+            name: "habluder adda",
+            category : "XYZ",
+            price : "20$"
+         },
+         bookCategory : "Basic",
+      },
+      {
+         bookId : 2,
+         bookDetails :{
+            name : "gobluder adda",
+            category : "ABC",
+            price : "40$",
+         },
+         bookCategory : "beginner"
       }
-   }
+   ]
 }
-
-console.log(students.address["city"])
-console.log(students[3333].name)
