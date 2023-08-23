a7609885f1dde51d6cd6189bc166952d50037378
chand-stack <chand.stack@gmail.com>
2023-08-23 20:38:13 +0600

    js practice

--- even.js
+++ even.js
@@ -53,5 +53,7 @@
 const numbers = [ 1, 5, 6, 4, 15]; 
 
 const players = [75, 65 , 67, 72, 55, 59];
-const selected = players.filter(x => x>60);
+// const selected = players.filter(x => x === 67);
+// const selected = players.filter(x => x%2 == 0 );
+const selected = players.filter(x => x%2 === 1)
 console.log(selected);
