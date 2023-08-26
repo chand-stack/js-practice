5436195c140d8fd7a5ab9ac48ab2d003d063c651
chand-stack <chand.stack@gmail.com>
2023-08-27 03:46:21 +0600

    js practice

--- practice.js
+++ practice.js
@@ -1,10 +1,2 @@
-const instructor =[
-   { name:'Nodi', age:28, position:'Senior'},
-   { name:'Akil', age:26, position:'Junior'},
-   { name:'Shobuj', age:30, position:'Senior'}
-]
-
-const names = instructor.filter(x => x.position === "Senior")
-
-const teacherName = names.map(x => x.name)
-console.log(teacherName)
+let first;
+console.log(first)
